a1d880bb33bbcc9ffd26e84d14e7c0d68560391d
sistemasiq <30322916+sistemasiq@users.noreply.github.com>
2024-09-10 15:04:15 +0000

    Se agrego VO para NombreUsuario

---
+++ backend/domain/src/main/java/dev/monkeys/backend/domain/vo/NombreUsuario.java
@@ -0,0 +1,13 @@
+package dev.monkeys.backend.domain.vo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@ToString
+@EqualsAndHashCode
+public class NombreUsuario {
+    private String value;
+
+}
