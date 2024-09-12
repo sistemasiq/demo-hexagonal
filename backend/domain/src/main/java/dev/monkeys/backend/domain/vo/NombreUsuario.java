7c1462c0ea53245c42dd657b6623cc7b9e0b9868
sistemasiq <30322916+sistemasiq@users.noreply.github.com>
2024-09-12 14:25:53 +0000

    Se cambio el VO de Nombre de usuario a un record

--- backend/domain/src/main/java/dev/monkeys/backend/domain/vo/NombreUsuario.java
+++ backend/domain/src/main/java/dev/monkeys/backend/domain/vo/NombreUsuario.java
@@ -1,13 +1,10 @@
 package dev.monkeys.backend.domain.vo;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+public record NombreUsuario(String value){
+    public NombreUsuario {
+        if ( (value.length()<8) || (value == null) ) {
+            throw new IllegalArgumentException("El nombre de usuario debe tener al menos 8 caracteres");
+        }
+    }
 
-@Data
-@ToString
-@EqualsAndHashCode
-public class NombreUsuario {
-    private String value;
-
-}
+} 
