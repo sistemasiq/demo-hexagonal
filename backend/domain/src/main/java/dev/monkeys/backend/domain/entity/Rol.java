a9e2fceb9e5dc04f10eebf819a4b8e3e7a305851
Diego Jared <djjaredev@gmail.com>
2024-09-24 14:23:44 +0000

    Convertir la clase Rol a un record

--- backend/domain/src/main/java/dev/monkeys/backend/domain/entity/Rol.java
+++ backend/domain/src/main/java/dev/monkeys/backend/domain/entity/Rol.java
@@ -2,18 +2,6 @@ package dev.monkeys.backend.domain.entity;
 
 import dev.monkeys.backend.domain.vo.Id;
 import dev.monkeys.backend.domain.vo.TipoRol;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
-@ToString
-public class Rol {
-    Id id;
-    TipoRol tipo;
+public record Rol(Id id, TipoRol tipo) {
 }
